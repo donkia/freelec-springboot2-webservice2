67479602b0ce752db448e3c7a896b1c4da088d6e
bhkim@naver.com <*wnddnjs0>
2021-08-30 16:25:51 +0900

    2장까지 내용

---
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -0,0 +1,27 @@
+package com.jojoldu.book.springboot.web;
+
+import com.jojoldu.book.springboot.Application;
+import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.*;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+
+        return new HelloResponseDto(name, amount);
+    }
+}
