cf5cf59b2463f6f34ab2b930bd60f3552bde7fe8
bhkim@naver.com <*wnddnjs0>
2021-09-09 20:02:24 +0900

    20210909 E-mail 발송 기능 제거

--- src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -5,18 +5,11 @@ import com.jojoldu.book.springboot.service.posts.PostsService;
 import com.jojoldu.book.springboot.service.posts.PostsUpdateRequestDto;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
-import jdk.management.resource.SimpleMeter;
 import lombok.RequiredArgsConstructor;
-import org.apache.logging.log4j.message.SimpleMessage;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.web.bind.annotation.*;
 
 import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,14 +17,10 @@ import javax.mail.internet.MimeMessage;
 public class PostsApiController {
 
     private final PostsService postsService;
-    private final EmailService emailService;
-
-    private final JavaMailSender mailSender;
 
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) throws MessagingException {
 
-      //emailService.mailSend();
         return postsService.save(requestDto);
     }
 
